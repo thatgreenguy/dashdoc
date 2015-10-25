9e2e2f64304c7926830b015456f8ec5ec63b13a6
Paul Green <thatgreenguy@gmx.co.uk>
2015-10-25 10:09:33 +0000

    WIP

--- wrk.sh
+++ wrk.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #
-# Stop container if running then remove it (it's a named container) then start it and drop into shell to work
+# Stop container if running 
+# then remove it (it's a named container) 
+# then start it and drop into shell to work
+# mount source code directory so can edit it from laptop via text editor
 #
-docker stop dashdoc && docker rm dashdoc && docker run -it --name dashdoc -v .:/src dlink/dash  /bin/bash
+docker stop dashdoc && \
+  docker rm dashdoc && \
+  docker run -it --name dashdoc -v `pwd`:/code dlink/dash  /bin/bash
