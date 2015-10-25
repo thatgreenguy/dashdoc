e0d68be94decf7981a495a5e17ac453c52ecfff1
thatgreenguy <thatgreenguy@gmx.co.uk>
2015-10-25 09:39:46 +0000

    Booting project

--- wrk.sh
+++ wrk.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+#
+# Stop container if running then remove it (it's a named container) then start it and drop into shell to work
+#
 docker stop dashdoc && docker rm dashdoc && docker run -it --name dashdoc -v .:/src dlink/dash  /bin/bash
