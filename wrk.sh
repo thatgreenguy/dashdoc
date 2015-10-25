f4b6bec8b6c1a867bffa531558588b46a4cfefda
Paul Green <thatgreenguy@gmx.co.uk>
2015-10-25 09:34:19 +0000

    Booting project

--- wrk.sh
+++ wrk.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-
-docker run -it --name dashdoc -v .:/src dlink/dash  /bin/bash
+docker stop dashdoc && docker rm dashdoc && docker run -it --name dashdoc -v .:/src dlink/dash  /bin/bash
